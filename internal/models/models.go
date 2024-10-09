5fffeb32ece1bd9a0dc244739aed69a0b8039536
Luca <lucaramos99@gmail.com>
2024-10-09 19:28:53 -0300

    Add models, controllers and repository for create, read, update and delete post

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,24 +3,23 @@ package models
 import (
 	"time"
 
-	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Post representa una publicación en el blog
 type Post struct {
-	ID        string    `json:"id" bson:"_id"`
-	Title     string    `json:"title" bson:"title"`
-	Content   string    `json:"content" bson:"content"`
-	Author    string    `json:"author" bson:"author"`
-	CreatedAt time.Time `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
-	Status    bool      `json:"status" bson:"status"`
-	UserId    string    `json:"user_id" bson:"user_id"`
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title     string             `json:"title" bson:"title"`
+	Content   string             `json:"content" bson:"content"`
+	Author    string             `json:"author" bson:"author"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	Status    bool               `json:"status" bson:"status"`
+	UserId    string             `json:"user_id" bson:"user_id"`
 }
 
 func NewPost(title, content, author, userid string) *Post {
 	return &Post{
-		ID:        uuid.New().String(), // Genera un nuevo UUID como ID
 		Title:     title,
 		Content:   content,
 		Author:    author,
